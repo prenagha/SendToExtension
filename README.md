4364f7a942f2ce13e5155738b2d9f583ae659335
Padraic Renaghan <prenagha@renaghan.com>
2013-05-06 16:47:31 -0400

    dotting i's

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ SendToExtension
 ===============
 
 Safari Extension to provide secondary menu options on anchor tags to send to Instpaper and Feed Wrangler
+
+The extension interface is through the context menu in Safari. When on a page in Safari trigger the context menu. The extension will attempt to find the "best" URL searching for the following in order:
+
+# The target of the context menu, if it is an anchor or is enclosed by an anchor, and that anchor has an href attribute
+
+# The link element relative with a type that appears to be an RSS feed
+
+# The URL of the page itself
+
+Installation
+============
+
+Download and install the SendTo Safari Extension
+
+After installing in Safari visit Preferences, Extensions, SendTo and
+enter your Instapaper user name and password (if you use them).
+
+Screenshots
+===========
+
+[Settings!](settings.png)
+
+[Context Menu!](screenshot.png)
+
--- SendTo.safariextension/end.js
+++ SendTo.safariextension/end.js
@@ -1,19 +1,54 @@
 // used as injected script to capture URL of target tag and
 // set into user info so that global page events can get the URL
 function sendto_handlecontextmenu_listener(event) {    
-  var data = null;
   if (event.target) {
-    //data = event.target.href;
-    data = sendto_findlink(event.target);
-    // handle relative URLs
-    if (data && data.indexOf('/') === 0) {
-      data = location.protocol + ':/' + url;
+    // look for an enclosing anchor tag to get the url
+    var data = sendto_findlink(event.target);
+
+    // otherwise look in the document for a feed url
+    if (!data) {
+      var links = document.getElementsByTagName('link');
+      for (var i=0; i < links.length; i++) {
+        var link = links[i];
+        if (link.getAttribute('rel').indexOf('alternate') >= 0) {
+          var linkType = link.getAttribute('type');
+          if (linkType.indexOf('application/rss+xml') >= 0 
+          ||  linkType.indexOf('text/xml') >= 0 
+          ||  linkType.indexOf('atom+xml') >= 0) {
+            data = link.getAttribute('href');
+          }
+        }
+      }
     }
-    //if (event.target.innerText)
-    //  data = data + " " + event.target.innerText;
+
+    // if no url found then use page url
+    if (!data  || data.length <= 2)
+      data = location.href;
+
+    // if still no url then done
+    if (!data || data.length <= 2)
+      return;
+                
+    if (data.indexOf('http') === 0) {
+      // we have a full url so good to go
+      
+    } else if (data.substring(8).indexOf('://') > 0) {
+      // non http URL so ignore it
+      return;
+      
+    } else if (data.indexOf('/') === 0) {
+      // we have a protocol relative url so add protocol and host back in front
+      data = location.protocol + '://' + location.host + url;
+    
+    } else {
+      // relative url so add protocol and base back
+      var firstSlash = location.href.substring(8).indexOf('/');
+      data = location.href.substring(0, 8 + firstSlash) + url;
+    }
+  
     console.log("sendto_handlecontextmenu_listener: " + data);
+    safari.self.tab.setContextMenuEventUserInfo(event, data);
   }
-  safari.self.tab.setContextMenuEventUserInfo(event, data);
 }
 
 function sendto_findlink(target) {
@@ -21,7 +56,8 @@ function sendto_findlink(target) {
   while (currentElement != null) {
     if (currentElement.nodeType == Node.ELEMENT_NODE 
     && currentElement.nodeName.toLowerCase() == 'a'
-    && currentElement.href && currentElement.href.length > 2) {
+    && currentElement.href 
+    && currentElement.href.length > 2) {
       return currentElement.href;
     }
     currentElement = currentElement.parentNode;
